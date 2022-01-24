6b41fe8d73bbda4957bb53451c3bcb4c5582061b
Newboy674 <Weirdgaming5@gmail.com>
2022-01-24 00:44:49 -0500

    Added The Epic Button, and updated other buttons

--- pythonProject/Dan_Bot/Dan_Bot2.py
+++ pythonProject/Dan_Bot/Dan_Bot2.py
@@ -11,6 +11,7 @@ from discord.ext import commands
 from discord.components import SelectOption
 from discord.commands import slash_command
 from discord.gateway import DiscordClientWebSocketResponse
+import random
 
 bot = discord.Bot()
 
@@ -20,8 +21,10 @@ async def on_ready():
 
 @bot.slash_command(name="roll", description= "Rolls a dice up to a specefied number", guild_ids=[792290455752146954])
 async def Dice_roll(ctx):
+
+    #Button formatting below
     btn = Button(
-        label = "This is a button?",
+        label = "Roll up to 6",
         style=discord.ButtonStyle.blurple,
     )
     btn2 = Button(
@@ -36,9 +39,25 @@ async def Dice_roll(ctx):
     view.add_item(btn)
     view.add_item(btn2)
     view.add_item(btn3)
+
+    #Stuff below is for button to be clickable
+    #Can be named whatever
+   #         l
+   #         v
+    number6 = random.randint(1, 5)
+    async def btn1click(interaction: discord.Interaction):
+        await interaction.response.send_message(f'Your number is, {number6}')
+
+    btn.callback = btn1click   #Assigns the button varible to the button click callback
+
     await ctx.respond("Does this work?", view=view)
+
+
+
+
+
 @bot.slash_command(name="epicbutton", description="Gives you the opportunity of a lifetime", guild_ids=[792290455752146954])
-async def epicbutton(ctx):
+async def epicbutton(ctx):      #Just print this and ill understand what each part does
     btn4 = Button(
     label="The Epic Button",
     url="https://www.youtube.com/watch?v=X_8Nh5XfRw0",
@@ -51,7 +70,17 @@ async def epicbutton(ctx):
         description="PRESS THIS AND EPIC WILL HAPPEN!!!",
         color=discord.Color.dark_gold()
     )
+    #Stuff below is formatting - not neccecary for button to work
+    embed.set_author(name="Epic Button")
+    embed.set_footer(text="Its very epic I promise")
+    embed.set_thumbnail(url="https://www.google.com/imgres?imgurl=https%3A%2F%2Fc.tenor.com%2Fhdl1-Mdo5XkAAAAC%2Fneko-arc.gif&imgrefurl=https%3A%2F%2Ftenor.com%2Fview%2Fneko-arc-gif-23482517&tbnid=gjiffNqQYFKmhM&vet=12ahUKEwjK1q_s4Mj1AhVPrHIEHUe_D3gQMygGegUIARDKAQ..i&docid=Va38oRCi6_dTWM&w=465&h=498&itg=1&q=neko%20arc&client=opera&ved=2ahUKEwjK1q_s4Mj1AhVPrHIEHUe_D3gQMygGegUIARDKAQ")
+    embed.set_image(url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6830ce29-acc6-4e8d-8a49-04dec20f2750/d10mkhp-e8cea004-1bf2-42e7-a4da-c8632f7b4706.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzY4MzBjZTI5LWFjYzYtNGU4ZC04YTQ5LTA0ZGVjMjBmMjc1MFwvZDEwbWtocC1lOGNlYTAwNC0xYmYyLTQyZTctYTRkYS1jODYzMmY3YjQ3MDYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.XCSBRMJLfKOU97fzV9M_rHhT-Q5FdCWxgT-2EvtiEoo")
+    #Stuff above is formatting
     await ctx.respond("EPIC BUTTON BELOW!!!", view=view, embed=embed)
+
+
+
+
 
 @bot.slash_command(name="say_my_name_test", description="Tells you your own name", guild_ids=[792290455752146954])
 async def say_my_name(ctx):
