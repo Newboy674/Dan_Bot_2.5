ef45574a0690ca90bf5bd6d52ac938b089138deb
Newboy674 <Weirdgaming5@gmail.com>
2022-02-11 22:13:07 -0500

    Adding an image editor??? using a class so I can do a bunch on images W.I.P
    Getting the time thing to work...
    
    Made image resize itself, use https://easy-pil.readthedocs.io/en/latest/easy_pil/easy_pil.editor.html to edit stuff better

--- pythonDay3/Python Day 13/Leetcode.py
+++ pythonDay3/Python Day 13/Leetcode.py
@@ -1,14 +1,12 @@
 ##https://leetcode.com/problems/two-sum/discuss/1754104/Not-the-most-efficient-but-very-easy-to-understand-in-Java
 
-
-def twoSum( nums, target):
+def twoSum(nums, target):  # -> List[int]:
         number_order1 = -1
         number_order2 = -1
 
         for number in nums:
             number_order1 = number_order1 + 1
             number1 = number
-            number_order2 = -1      #Resets second number
 
             for number in nums:
                 number_order2 = number_order2 + 1
@@ -26,6 +24,4 @@ def twoSum( nums, target):
 test_number_list=[5, 7, 2, 9]
 target = 11
 
-Result = twoSum(test_number_list, target)
-
-print(Result)
+twoSum(test_number_list, target)
