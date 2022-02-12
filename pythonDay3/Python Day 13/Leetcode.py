cdb6faf7768f8b38f0ea47fedbc77148242bc5e3
Newboy674 <Weirdgaming5@gmail.com>
2022-02-12 00:17:26 -0500

    leetcode day 1, WORKS (lol no), beTTER TOO!!

--- pythonDay3/Python Day 13/Leetcode.py
+++ pythonDay3/Python Day 13/Leetcode.py
@@ -1,21 +1,18 @@
 ##https://leetcode.com/problems/two-sum/submissions/
+
 class Solution:
-    def __init__(self, nums, target):
-        self.nums = nums
-        self.target = target
 
     @classmethod     #WHY DID This FIX EVERYTHING whAT
+
     def twoSum(self, nums, target):  # -> List[int]:
-            number_order1 = -1
-            number_order2 = -1
 
             for number in nums:
-                    number_order1 = number_order1 + 1
                     number1 = number
+                    number_order1 = nums.index(number)
 
                     for number in nums:
-                            number_order2 = number_order2 + 1
                             number2 = number
+                            number_order2 = nums.index(number)
 
                             if number1 + number2 == target:
                                 result = [number_order1, number_order2]
@@ -24,11 +21,12 @@ class Solution:
 
 
 
-     #This took a while to add holy... makes the class a callable?
 
-test_number_list=[5, 7, 2, 9]
+test_number_list=[5,2,9,5,7]
 target = 11
 
 Result = Solution.twoSum(test_number_list, target)
 
 print(Result)
+
+
