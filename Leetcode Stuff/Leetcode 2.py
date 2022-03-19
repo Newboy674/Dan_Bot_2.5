ab87aae92a4d9977aa80d7335ab80601d2aaf0d1
Newboy674 <Weirdgaming5@gmail.com>
2022-03-18 21:39:12 -0400

    ugh, idea 2 of leetcode 2 parrt4t 2

--- Leetcode Stuff/Leetcode 2.py
+++ Leetcode Stuff/Leetcode 2.py
@@ -1,5 +1,28 @@
 ##https://leetcode.com/problems/roman-to-integer/
 
+class romanvalue:
+    def __init__(self, symbol):
+        self.Symbol = symbol
+
+    def value(self):
+        if "I" in self.symbol:
+         absolute_value = 1
+        if "V" in self.symbol:
+         absolute_value = 5
+        if "X" in self.symbol:
+            absolute_value = 10
+        if "L" in self.symbol:
+          absolute_value = 50
+        if "C" in self.symbol:
+          absolute_value = 100
+        if "D" in self.symbol:
+           absolute_value = 500
+        if "M" in self.symbol:
+          absolute_value = 1000
+        return absolute_value
+
+
+
 class Solution:
     @classmethod
     def Roman(self, s):
@@ -7,42 +30,31 @@ class Solution:
 
         if "M" in s:
 
-            Answer = Answer + 1000
-
         if "D" in s:
-            Answer = Answer + 500
 
         if "C" in s:
-            Answer = Answer + 100
 
         if "L" in s:
-            Answer = Answer + 50
 
         if "X" in s:
-            Answer = Answer + 10
 
         if "V" in s:
+
             V_counter = s.count("V")
 
-            Answer = Answer + 5
+            V_tuple = s.partition(s)
+
+            V_before = V_tuple[0]
+
+            V_after = V_tuple[2]
+
 
         if "I" in s:
 
-            I_counter = s.count("I")    #I_counter is equal to the amount of I's in the string (s)
-            afterV = s.count ("VI")   #Will be 1 if after V, Will be 0 if not. This works because you cannot have "IVI" in roman numerals
+           I_tuple = s.partition(I)
 
-            if I_counter == 1:
-                Answer = Answer + 1
 
-            elif I_counter != 1:
-
-                if afterV == 1:
-                    Answer = Answer + I_counter
-
-                else:
-                 Answer = Answer - I_counter
-
-            return Answer
+        return Answer
 
 
 #Test
@@ -50,3 +62,6 @@ class Solution:
 Ans = Solution.Roman("LIIIV")
 
 print(Ans)
+
+
+##given up, a case of "MCMXCIV" confused me, as the mcm wont work wif me code
